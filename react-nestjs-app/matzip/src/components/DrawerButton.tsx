ded76451f3267f2636ebf2e659f965d325c72d59
young_person <49819098+youngPerson1999@users.noreply.github.com>
2025-11-06 12:31:18 +0900

    icon 추가

--- react-nestjs-app/matzip/src/components/DrawerButton.tsx
+++ react-nestjs-app/matzip/src/components/DrawerButton.tsx
@@ -1,17 +1,25 @@
 import {DrawerNavigationProp} from '@react-navigation/drawer';
 import {useNavigation} from '@react-navigation/native';
-import {Pressable, Text} from 'react-native';
+import {Pressable, StyleSheet} from 'react-native';
 import {MainDrawerParamList} from '../types/navigation';
+import Ionicons from '@react-native-vector-icons/ionicons';
+import {colors} from '../constants/colors';
 
 type Navigation = DrawerNavigationProp<MainDrawerParamList>;
 
-function DrawerButton() {
+function DrawerButton({color = colors.BLACK}: {color?: string}) {
   const navigation = useNavigation<Navigation>();
   return (
-    <Pressable onPress={() => navigation.openDrawer()}>
-      <Text style={{fontSize: 20}}>서랍</Text>
+    <Pressable style={styles.container} onPress={() => navigation.openDrawer()}>
+      <Ionicons name="menu" size={25} color={color} />
     </Pressable>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 12,
+  },
+});
 
 export default DrawerButton;
--- react-nestjs-app/matzip/src/navigations/DrawerNavigation.tsx
+++ react-nestjs-app/matzip/src/navigations/DrawerNavigation.tsx
@@ -6,31 +6,65 @@ import {FeedStack} from './FeedNavigation';
 import DrawerButton from '../components/DrawerButton';
 import {colors} from '../constants/colors';
 import CustomDrawerContent from '../components/CustomDrawerContent';
+import {MainDrawerParamList} from '../types/navigation';
+import FontAwesome6 from '@react-native-vector-icons/fontawesome6';
+
+type DrawerIconName = 'map' | 'book' | 'calendar';
+
+function DrawerIcons(routeName: keyof MainDrawerParamList, focused: boolean) {
+  let iconName: DrawerIconName;
+  switch (routeName) {
+    case 'Map':
+      // iconName = focused ? 'map' : 'map-outline';
+      iconName = 'map';
+      break;
+    case 'Feed':
+      // iconName = focused ? 'list' : 'list-outline';
+      iconName = 'book';
+      break;
+    case 'Calendar':
+      // iconName = focused ? 'calendar' : 'calendar-outline';
+      iconName = 'calendar';
+      break;
+  }
+  return (
+    <FontAwesome6
+      name={iconName}
+      iconStyle="solid"
+      size={20}
+      color={focused ? colors.WHITE : colors.GRAY_300}
+    />
+  );
+}
 
 const MainDrawer = createDrawerNavigator({
-  screenOptions: {
-    drawerStyle: {
-      width: '60%',
-      backgroundColor: colors.WHITE,
-    },
-    drawerLabelStyle: {
-      fontWeight: '600',
-    },
-    drawerItemStyle: {
-      borderRadius: 15,
-    },
-    drawerType: 'front',
-    drawerActiveTintColor: colors.WHITE,
-    drawerActiveBackgroundColor: colors.PINK_700,
-    drawerInactiveTintColor: colors.GRAY_500,
-    drawerInactiveBackgroundColor: colors.GRAY_100,
-    headerTitleAlign: 'center',
-    headerBackButtonDisplayMode: 'minimal',
-    headerTintColor: colors.BLACK,
-    headerStyle: {
-      backgroundColor: colors.WHITE,
-      shadowColor: colors.GRAY_500,
-    },
+  screenOptions: ({route}) => {
+    return {
+      drawerStyle: {
+        width: '60%',
+        backgroundColor: colors.WHITE,
+      },
+      drawerLabelStyle: {
+        fontWeight: '600',
+      },
+      drawerItemStyle: {
+        borderRadius: 15,
+      },
+      drawerType: 'front',
+      drawerActiveTintColor: colors.WHITE,
+      drawerActiveBackgroundColor: colors.PINK_700,
+      drawerInactiveTintColor: colors.GRAY_500,
+      drawerInactiveBackgroundColor: colors.GRAY_100,
+      drawerIcon: ({focused}) => DrawerIcons(route.name, focused),
+
+      headerTitleAlign: 'center',
+      headerBackButtonDisplayMode: 'minimal',
+      headerTintColor: colors.BLACK,
+      headerStyle: {
+        backgroundColor: colors.WHITE,
+        shadowColor: colors.GRAY_500,
+      },
+    };
   },
   screens: {
     Map: {
