02c7bf8d66615b5b670b4b7116084aa6aa0834ea
young_person <49819098+youngPerson1999@users.noreply.github.com>
2025-11-07 11:24:24 +0900

    3-7 encryptStorage 추가

---
+++ react-nestjs-app/matzip/src/utils/encryptStorage.ts
@@ -0,0 +1,23 @@
+import EncryptedStorage from 'react-native-encrypted-storage';
+
+async function setEncryptStorage<T>(key: string, data: T) {
+  await EncryptedStorage.setItem(key, JSON.stringify(data));
+}
+
+async function getEncryptStorage<T>(key: string): Promise<T | null> {
+  const storagedData = await EncryptedStorage.getItem(key);
+  return storagedData ? (JSON.parse(storagedData) as T) : null;
+}
+
+async function removeEncryptStorage(key: string): Promise<void> {
+  const data = await getEncryptStorage(key);
+  if (data) {
+    await EncryptedStorage.removeItem(key);
+  }
+}
+
+export default {
+  set: setEncryptStorage,
+  get: getEncryptStorage,
+  remove: removeEncryptStorage,
+};
